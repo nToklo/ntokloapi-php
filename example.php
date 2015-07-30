933b3a254fce3d359fd7b565b27b601e4441ded7
Fu <fu.hoang@ntoklo.com>
2015-07-30 14:15:38 +0100

    update example.php

--- example.php
+++ example.php
@@ -1,61 +1,80 @@
 <?php
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
+//require the nToklo api
+require '/path/to/Ntokloapi/NtokloApi.php';
+
+// create a ntoklo pai instance and pass the key and secret for more infomation got to: http://docs.ntoklo.com/start.php
+$api = new Ntokloapi\NtokloApi( 'key', 'secret' );
 
 
-
-$data = array('version' => '1.2',
-              'user' => array('user_id' => '112'),
-              'product' => array("id" => "10201",
-                                 "name" => "Gabardine A-line skirt",
-                                 "category" => "Womens > Skirts",
-                                 "currency" => "GBP",
-                                 "unit_sale_price"=> 98),
-              'events' => array((object)['category' => 'conversion_funnel', 'action' => 'browse' ])
-              );
-
-$_data_product = array('version' => '1.2',
-                       'product' => array("id" => "10201",
-                                     "name" => "Gabardine A-line skirt",
-                                     "category" => "Womens > Skirts",
-                                     "currency" => "GBP",
-                                     "unit_sale_price" => 98)
-                       );
-
-//print_r(json_encode($data));
-//print_r(json_encode($_data_product));
+/**
+ * post Event to the nToklo api,
+ * For more info go to: http://docs.ntoklo.com/start.php/api_reference:events
+ * @param pass an json decode object as an args
+ * @return bool
+ */
+$api->postEvent($data);
 
 
-require('ntokloapi/NtokloApi.php');
-$api = new Ntokloapi\NtokloApi('YzBlYmIzYWMtZDg0Zi00MTc0LWEyZTgtMzNiMGU3ZmU1MTA3', 'OGNmZDkwZGMtNTdlMi00ZWNmLWFjNzAtMmE3MTU4YjQ2MzM2' );
-echo $api->postEvent($data);
-echo $api->postProduct($_data_product);
-//echo $api->recommendations('1', '10201', null, null);
-//echo $api->chart(null, null, null, null, 'DAILY');
-//echo $api->addBlacklist('10201');
-//echo $api->removeBlacklist('10201');
-//echo $api->fetchBlacklist();
+/**
+ * post products to the nToklo api,
+ * For more info go to: http://docs.ntoklo.com/start.php/api_reference:products
+ * @param pass an json decode object as an args
+ * @return bool
+ */
+$api->postProduct($_data_product);
 
+
+/**
+ * fetch the recommendations from nToklo api
+ * For more info go to: http://docs.ntoklo.com/start.php/api_reference:recommendations
+ * @param string $userId optional.
+ * @param string $productId optional.
+ * @param string $scope optional.
+ * @param string $value optional.
+ * @return Json
+ */
+$api->recommendations($userId, $productId, $scope, $value);
+
+
+/**
+ *This function will fetch the charts from nToklo api
+ * For more info go to: http://docs.ntoklo.com/start.php/api_reference:charts
+ * @param string $timestamp Optional.
+ * @param string $scope Optional.
+ * @param string $value Optional.
+ * @param string $action Optional
+ * @param string $tw Optional.
+ * @param srting $maxItems Optional.
+ * @return Json object
+ */
+$api->chart($timestamp, $scope, $value, $action, $tw, $maxItems);
+
+
+ /**
+  * Post a product id to nToklo api to blacklist
+  * For more info go to: http://docs.ntoklo.com/start.php/api_reference:blacklist
+  * @param string $productId
+  * @return bool
+  */
+$api->addBlacklist($product);
+
+
+ /**
+  * Remove a product id from blacklist
+  * For more info go to: http://docs.ntoklo.com/start.php/api_reference:blacklist
+  * @param string $productId
+  * @return bool
+  */
+$api->removeBlacklist($productId);
+
+
+ /**
+  * Fetch all product from blacklist
+  * For more info go to: http://docs.ntoklo.com/start.php/api_reference:blacklist
+  * @param string $productId
+  * @return bool
+  */
+$api->fetchBlacklist();
 
 ?>
-
-<!doctype html>
-
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-
-  <title>Api Test</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
-
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
-</head>
-
-<body>
-    <p>Hello world!</p>
-
-</body>
-</html>
