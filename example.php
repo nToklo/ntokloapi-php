7661b3caadf99522a20251494302601e57dcd7fd
Fu <fu.hoang@ntoklo.com>
2015-08-25 13:57:46 +0100

    unit testing added

--- example.php
+++ example.php
@@ -33,10 +33,10 @@ $data_product = array('version' => '1.2',
 //require 'app/NtokloApi.php';
 require "vendor/autoload.php";
 $api = new NtokloApi('OTNmMjlhZmUtZmQ4Yy00MjQ4LThjODAtNzBjMjJlODRjYjVh', 'YzM0OTlhNDAtZGMxZS00Yzg4LWEyZjAtOWVlMmM5NGIyZjM3');
-echo $api->postEvent($data);
+//echo $api->postEvent($data);
 //echo $api->postProduct($data_product);
 //echo $api->getProduct('192');
-//echo $api->recommendations($userId = '112', $productId = 'productId', $scope = null, $value = null);
+echo $api->recommendations($userId = '112', $productId = 'productId', $scope = null, $value = null);
 //echo $api->chart($timestamp = null, $scope = null, $value = null, $action = null, $tw = 'DAILY', $maxItems = null);
 //echo $api->addBlacklist("192");
 //echo $api->removeBlacklist("168");
--- test/NtokloApiTest.php
+++ test/NtokloApiTest.php
@@ -1,20 +1,72 @@
 <?php
 class NtokloApiTest extends PHPUnit_Framework_TestCase {
 
+    public function setup(){
+        $this->api = new NtokloApi('OTNmMjlhZmUtZmQ4Yy00MjQ4LThjODAtNzBjMjJlODRjYjVh', 'YzM0OTlhNDAtZGMxZS00Yzg4LWEyZjAtOWVlMmM5NGIyZjM3');
+
+    }
+
+    public static function jsonData(){
+        return array(
+                "version"   => "1.2",
+                "user"      => array("user_id" => "112"),
+                "product"   => array(
+                                "id"              => "10201",
+                                "name"            => "Gabardine A-line skirt",
+                                "category"        => "Womens > Skirts",
+                                "currency"        => "GBP",
+                                "unit_sale_price" => 98),
+                "events"    => array((object)["category" => "conversion_funnel", "action" => "browse" ])
+                );
+    }
+
+
     public function testPostEvent()
     {
+        $this->assertEquals(true, $this->api->postEvent($this->jsonData()));
+    }
 
-        $data = array('version' => '1.2',
-              'user' => array('user_id' => '112'),
-              'product' => array("id" => "10201",
-                                 "name" => "Gabardine A-line skirt",
-                                 "category" => "Womens > Skirts",
-                                 "currency" => "GBP",
-                                 "unit_sale_price"=> 98),
-              'events' => array((object)['category' => 'conversion_funnel', 'action' => 'browse' ])
-              );
-        $api = new NtokloApi('OTNmMjlhZmUtZmQ4Yy00MjQ4LThjODAtNzBjMjJlODRjYjVh', 'YzM0OTlhNDAtZGMxZS00Yzg4LWEyZjAtOWVlMmM5NGIyZjM3');
-        $this->assertEquals(true, $api->postEvent($data));
+    public function testPostProduct(){
+        $data_product = array("version" => "1.2",
+                              "product" => array(
+                                            "id"              => "886",
+                                            "name"            => "KOTORI BAG",
+                                            "category"        => "all",
+                                            "currency"        => "GBP",
+                                            "unit_sale_price" => 30)
+                            );
+      $this->assertEquals(true, $this->api->postProduct($data_product));
+    }
 
+    public function testGetProduct(){
+        $response = $this->api->getProduct('192');
+        $this->assertNotNull($response);
+    }
+
+    public function testRecommendations(){
+        $response = $this->api->recommendations($userId = null, $productId = '184', $scope = null, $value = null);
+        print $response;
+        $this->assertNotNull($response);
+    }
+
+    public function testCharts(){
+        $response = $this->api->chart($timestamp = null, $scope = null, $value = null, $action = null, $tw = 'DAILY', $maxItems = null);
+        print $response;
+        $this->assertNotNull($response);
+    }
+
+    public function testAddBlacklist(){
+        $response = $this->api->addBlacklist('199');
+        $this->assertEquals(true, $response);
+    }
+
+    public function testRemoveBlacklist(){
+        $response = $this->api->removeBlacklist('199');
+        $this->assertEquals(true, $response);
+    }
+
+    public function testFetchBlacklist(){
+        $response = $this->api->fetchBlacklist();
+        $this->assertNotNull($response);
     }
 }
